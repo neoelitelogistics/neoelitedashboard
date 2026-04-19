e829f231fb2922b44e690113838a0e7579720b85
Neoelite Admin <admin@neoelitelogistics.com>
2026-04-19 18:58:29 +0530

    Fix dashboard caching with force-dynamic

--- src/app/supervisor/page.js
+++ src/app/supervisor/page.js
@@ -3,6 +3,8 @@ import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import SupervisorClient from './SupervisorClient';
 
+export const dynamic = 'force-dynamic';
+
 export default async function SupervisorDashboard() {
   const cookieStore = await cookies();
   const authCookie = cookieStore.get('auth_user');
